9ae1abb69f2d7731b92d4c321352757209a8f801
Anwesh43 <anweshthecool0@gmail.com>
2020-11-20 14:16:45 +0530

    adding SBBNode class

--- index.ts
+++ index.ts
@@ -132,3 +132,45 @@ class Animator {
         }
     }
 }
+
+class SBBNode {
+
+    prev : SBBNode 
+    next : SBBNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new SBBNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawSBBNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : SBBNode {
+        var curr : SBBNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
