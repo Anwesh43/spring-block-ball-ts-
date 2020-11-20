40376c8f1c054db606d43c64433f2ddd75cad289
Anwesh43 <anweshthecool0@gmail.com>
2020-11-20 14:20:53 +0530

    adding SpringBlockBall class

--- index.ts
+++ index.ts
@@ -174,3 +174,26 @@ class SBBNode {
         return this 
     }
 }
+
+class SpringBlockBall {
+
+    curr : SBBNode = new SBBNode(0)
+    dir : number = 1 
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : Function) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : Function) {
+        this.curr.startUpdating(cb)
+    }
+}
